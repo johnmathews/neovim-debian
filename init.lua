ab463a0fb265fb20041d5bb44986916c30f29e2a
John Mathews <john.mathews@air-up.com>
2022-12-26 22:35:57 +0100

    remove commented code

--- init.lua
+++ init.lua
@@ -1,11 +1,6 @@
 vim.cmd("let g:python3_host_prog = expand('~/.pyenv/versions/3.10.0/envs/nvim/bin/python3')")
 vim.cmd("let g:node_host_prog = expand('~/.nvm/versions/node/v17.9.0/bin/node')")
 
--- use nvim 0.7 experimental filetype detection. should give faster startup time
--- https://gpanders.com/blog/whats-new-in-neovim-0-7/
--- vim.cmd("let g:do_filetype_lua = 1")
--- vim.cmd("let g:did_load_filetypes = 0")
-
 require("options")
 require("plugins")
 require("plugins.lsp")
