cfc433ff924fe3358975f03f08bd4a659b5808a9
John Mathews <john.mathews@air-up.com>
2022-12-27 19:30:27 +0100

    tidy

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -36,15 +36,26 @@ local on_attach = function(client, bufnr)
   local opts = { noremap = true, silent = true }
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
+  -- HANDLED BETTER BY LSPSAGA.
+  --   vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+  --   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+  --   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+  --   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  --   See `:help vim.lsp.*` for documentation on any of the below functions
+  --   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  --   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  --   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts) -- view a list of errors and warnings
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+
+
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-
-  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 
   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
@@ -58,21 +69,6 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
--- local on_attach = function(client, bufnr)
---   local opts = { noremap=true, silent=true }
---   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-
---   vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
---   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
---   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
---   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-
---   -- Mappings.
---   -- See `:help vim.lsp.*` for documentation on any of the below functions
---   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
---   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
---   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
--- end
 
 local lsp_flags = {
   debounce_text_changes = 150,
