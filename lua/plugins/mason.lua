9888e446320c4c7e3854339c2150a77b028221ba
John Mathews <john.mathews@air-up.com>
2022-12-27 19:25:41 +0100

    tidy up

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -36,10 +36,13 @@ local on_attach = function(client, bufnr)
   local opts = { noremap = true, silent = true }
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+
   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
   vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 
@@ -48,46 +51,30 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
   vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
 
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
--- local on_attach = function(client, bufnr)
 
+-- local on_attach = function(client, bufnr)
 --   local opts = { noremap=true, silent=true }
+--   local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
 --   vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+--   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 --   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 --   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
---   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-
---   -- Enable completion triggered by <c-x><c-o>
---   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
 --   -- Mappings.
 --   -- See `:help vim.lsp.*` for documentation on any of the below functions
---   local bufopts = { noremap=true, silent=true, buffer=bufnr }
---   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
---   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 --   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
---   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
---   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
---   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
---   vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
---   vim.keymap.set('n', '<leader>wl', function()
---     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---   end, bufopts)
---   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
 --   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
 --   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
---   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
---   vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
 -- end
 
 local lsp_flags = {
-  -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
 
