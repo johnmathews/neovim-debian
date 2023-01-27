7050680ac7ccc584359b190c179089c2fe152a11
John Mathews <john.mathews@air-up.com>
2023-01-27 14:35:25 +0100

    lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -18,10 +18,6 @@ local function Row_max_row()
   return "r" .. row .. "/" .. max_row
 end
 
-local function total_rows()
-  return vim.api.nvim_buf_line_count(0)
-end
-
 local function show_filename()
   local filename = vim.fn.expand("%:t")
   return "/" .. filename
@@ -30,16 +26,6 @@ end
 local function show_filepath()
   local parent = vim.fn.expand("%:h")
   return parent
-end
-
--- progress function
-local progress = function()
-  local current_line = vim.fn.line(".")
-  local total_lines = vim.fn.line("$")
-  local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-  local line_ratio = current_line / total_lines
-  local index = math.ceil(line_ratio * #chars)
-  return chars[index]
 end
 
 lualine.setup({
@@ -54,12 +40,12 @@ lualine.setup({
     always_divide_middle = true,
   },
   tabline = {
-    lualine_a = { progress, require('auto-session-library').current_session_name, Row_max_row, Current_col, "mode" },
+    lualine_a = { require('auto-session-library').current_session_name, Row_max_row, Current_col, "mode" },
     lualine_b = { { "branch", padding = { left = 3, right = 1 } }, { "diff", padding = { left = 1, right = 3 } } },
     lualine_c = { { show_filepath, padding = { left = 2, right = 0 }, color = { fg = "#000000", bg = "#39FF14" } },
       { show_filename, color = { fg = "#000000", bg = "#39FF14" }, padding = { left = 0, right = 2 } },
       { "diagnostics", padding = { left = 2, right = 2 } }, "lsp_progress" },
-    lualine_x = { "encoding", "fileformat", "filetype", { total_rows, color = { fg = '#FF647F' } } },
+    lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = {},
     -- https://www.reddit.com/r/neovim/comments/q2s3t1/how_to_get_current_filename_relative_to_project/
     -- https://stackoverflow.com/questions/4525261/getting-relative-paths-in-vim
@@ -78,7 +64,6 @@ lualine.setup({
     'quickfix',
     'nvim-tree',
     'toggleterm',
-    -- 'symbols-outline',
     'nvim-dap-ui',
     'mundo',
     'man',
