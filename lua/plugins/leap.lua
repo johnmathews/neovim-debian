a73e2c80698764017601061e4a127bb4c8dc0454
John Mathews <john.mathews@air-up.com>
2023-01-28 10:07:17 +0100

    remove a leap keybind

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,5 +1,5 @@
 require('leap').add_default_mappings()
 
 -- see the FAQ
--- vim.keymap.del({ 'x', 'o' }, 'x')
--- vim.keymap.del({ 'x', 'o' }, 'X')
+vim.keymap.del({ 'x', 'o' }, 'x')
+vim.keymap.del({ 'x', 'o' }, 'X')
