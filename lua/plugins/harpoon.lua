8262d0e9d58f81d06478e7bf6e96a33b41ae2b76
John Mathews <john.mathews@air-up.com>
2023-02-03 15:51:28 +0100

    rename test-lua to vim-test.lua

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,7 +2,7 @@ require("harpoon").setup()
 
 local map = vim.api.nvim_set_keymap
 map("n", "ga", ":lua require('harpoon.mark').add_file()<CR>:echom 'added to Harpoon'<CR>", KeymapOptions("Harpoon Add"))
-map("n", "gh", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", KeymapOptions("Harpoon Toggle"))
+map("n", "<leader>h", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", KeymapOptions("Harpoon Toggle"))
 map("n", "gn", ":lua require('harpoon.ui').nav_next()<CR>", KeymapOptions("Harpoon Next"))
 map("n", "gp", ":lua require('harpoon.ui').nav_prev()<CR>", KeymapOptions("Harpooon Previous"))
 map("n", "<Tab>1", ":lua require('harpoon.ui').nav_file(1)<CR>", KeymapOptions("Harpoon buffer"))
--- lua/plugins/indentlineDELETE.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd([[
-  let g:indentLine_char_list = ['|', '¦', '┆', '┊']
-  " let g:indentLine_setConceal = 0
-  " let g:indentLine_conceallevel = 1
-]])
