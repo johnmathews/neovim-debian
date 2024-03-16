41155df60325270e8e10a37a2a89f090a5fa54bb
John Mathews <mthwsjc@gmail.com>
2024-03-16 20:31:49 +0100

    add foldlevel=99 to treesitter folding autocommand

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -10,11 +10,12 @@
 
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = {"python", "yaml", "lua"},
-    callback = function()
-        vim.opt_local.foldmethod = "expr"
-        vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
-    end,
+  pattern = { "python", "yaml", "lua" },
+  callback = function()
+    vim.opt_local.foldmethod = "expr"
+    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt_local.foldlevel = 99
+  end,
 })
 
 -- highlight on yank
