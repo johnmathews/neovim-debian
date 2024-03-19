d4a7c5d828e5cb62b55bac556534ebccabc82320
John Mathews <mthwsjc@gmail.com>
2024-03-19 22:01:59 +0100

    add some syntax highlighting groups to init.lua

--- init.lua
+++ init.lua
@@ -63,6 +63,30 @@ require("autocmd")
 -- load custom snippets. dont remove this.
 require("luasnip.loaders.from_lua").load({ paths = "./lua/snippets" })
 
+-- Ensure the setup_env_syntax function is local to avoid polluting the global namespace
+local function setup_env_syntax()
+    vim.cmd [[
+    syntax clear
+    syntax match EnvComment "#.*" contains=@Spell
+    syntax match EnvKey "^\s*\w\+" nextgroup=EnvSeparator
+    syntax match EnvSeparator "=" contained nextgroup=EnvValue
+    syntax match EnvValue "[\w@-]\+" contained
+
+    highlight EnvComment ctermfg=Cyan guifg=Cyan
+    highlight EnvKey ctermfg=White guifg=White
+    highlight EnvSeparator ctermfg=Yellow guifg=Yellow
+    highlight EnvValue ctermfg=Red guifg=Red
+    ]]
+end
+
+-- Use vim.api.nvim_create_autocmd to set up the autocommand
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "*.env",
+    callback = function()
+        setup_env_syntax()
+    end,
+})
+
 -- use the following to see what highlight group the text under the cursor is part of
 -- :exe 'hi '.synIDattr(synstack(line('.'), col('.'))[-1], 'name')
 
